adf706ff576bde9cf699b43820548c2695e65b3f
iuyhcdfs <iuyhcdfs@gmail.com>
2016-12-06 11:05:03 +1100

    up to q2 part 1

--- 1.py
+++ 1.py
@@ -20,20 +20,21 @@ for line in array:
         compass.rotate(-1)
     if turn == "R":
         compass.rotate(1)
-    for x in range(0,4):
-        for y in range(1,(int(dist)+1))
-            travel[x] += compass[x]
+    for i in range(0,4):
+        for whateverrrr in range(0,int(dist)):
+            if compass[i] == 0:
+                break
+            travel[i] += compass[i]
             x = travel[1] - travel[3]
             y = travel[0] - travel[2]
-            been[str(x)+","+str(y)] = 1;
             if (str(x)+","+str(y)) in been and first == 1:
                 print ("2nd visit to "+str(x)+" and "+str(y)) 
-                print ("manhattan distance is "+str(x+y))
+                print ("manhattan distance is "+str(abs(x)+abs(y)))
                 first = 0
-    
+            been[str(x)+","+str(y)] = 1;
 
 x = travel[1] - travel[3]
 y = travel[0] - travel[2]
 print ("last x = "+str(x))
 print ("last y = "+str(y))
-print ("manhattan = "+str(x+y))
+print ("manhattan = "+str(abs(x)+abs(y)))
