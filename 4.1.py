0fe73eb2ce834a1d1a16bd4751da31f0852c6214
iuyhcdfs <iuyhcdfs@gmail.com>
2016-12-07 13:41:03 +1100

    ez pz but off to do other things cya aoc for a bit

--- 4.1.py
+++ 4.1.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3.5
 
 import sys
+import re
+for stdl in sys.stdin:
+    m = re.match(r"^(.*)-(...)\[(.....)\]$", stdl)
+    if m:
+        print(m.group(1))
+        print(m.group(2))
+        print(m.group(3))
 
